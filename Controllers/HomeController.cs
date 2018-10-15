75fffaacd11f3e372e802b939a9ed0726191181d
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-10-15 18:21:20 +0200

    First controller.
    A simple class with a method returning a string. I have activated MVC using the middleware UseMVC() in Startup class.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+namespace storeroom_web_netcore.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from HomeController";
+        }
+    }
+}
