db8af8b8df44327e459df0d3b91f257bf6a32946
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-10-16 17:34:44 +0200

    Routing with Attributes
    In this commit I have refactorized the AboutController to using the attribute Route. In this case, we have configured the routes to /about and the controller will invoke to Phone(), and /about/address and the controller will invoke to Address().

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace storeroom_web_netcore.Controllers
 {
+    
+    [Route("about")]
     public class AboutController
     {
+        [Route("")]
         public string Phone() 
         {
             return "+34666965434";
         }
 
+        [Route("address")]
         public string Address()
         {
             return "My Address";
