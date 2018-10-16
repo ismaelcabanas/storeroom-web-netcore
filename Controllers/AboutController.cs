81da036a906c8d3fef54bc96f9631beb6e0e8677
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-10-16 17:22:52 +0200

    Another controller example
    We can invoke the request /about/phone or /about/address

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+namespace storeroom_web_netcore.Controllers
+{
+    public class AboutController
+    {
+        public string Phone() 
+        {
+            return "+34666965434";
+        }
+
+        public string Address()
+        {
+            return "My Address";
+        }
+    }
+}
